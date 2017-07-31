13245ce66ea1d7c2166db16467824575f61c6598
UtsavPreet <utsavpreet@gmail.com>
2017-07-31 11:29:47 +0530

    Added Body Parser

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// routes will go here
+app.get('/api/users', function(req, res) {
+  var user_id = req.param('id');
+  var token = req.param('token');
+  var geo = req.param('geo');  
+
+  res.send(user_id + ' ' + token + ' ' + geo);
+});
 
 
 app.listen(port);
